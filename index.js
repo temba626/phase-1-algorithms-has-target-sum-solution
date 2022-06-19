0264b5f459fbd95310cdceb0abcba2d3a307ef04
Brandon Temba <brandontemba560@gmail.com>
2022-06-19 21:05:33 +0300

    commit tree 6d2f01caee9c4e21b4138c3d5213fe511be1ad2d

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = new Set();
+  for (const number of array) {
+    const complement = target - number;
+
+    if (seenNumbers.has(complement)) return true;
+
+    seenNumbers.add(number);
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+  an array list comes in here
 */
 
 /* 
   Add your pseudocode here
+
+  the pseudo code comes in here
 */
 
 /*
